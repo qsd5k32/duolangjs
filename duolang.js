6b5a7742bec77d091894a25bcb476e0fc32ea622
belgacemi mohamed seddik <sed75dik@gmail.com>
2018-01-05 00:57:47 +0100

    Update duolang.js

--- duolang.js
+++ duolang.js
@@ -1,8 +1,8 @@
-var Language = /** @class */ (function () {
-    function Language() {
+var duoLang = /** @class */ (function () {
+    function duoLang() {
         this.Words = {};
     }
-    Language.prototype.getCookie = function (name) {
+    duoLang.prototype.getCookie = function (name) {
         var nameLenPlus = (name.length + 1);
         return document.cookie
             .split(';')
@@ -14,22 +14,22 @@ var Language = /** @class */ (function () {
             return decodeURIComponent(cookie.substring(nameLenPlus));
         })[0] || null;
     };
-    Language.prototype.getLang = function (name) {
+    duoLang.prototype.getLang = function (name) {
         if (name === void 0) { name = 'lang'; }
         if (this.getCookie(name) === null)
             return 'en';
         if (this.getCookie(name) != null)
             return this.getCookie(name);
     };
-    Language.prototype.setWords = function (data, lang) {
+    duoLang.prototype.setWords = function (data, lang) {
         this.Words[lang] = data;
     };
-    Language.prototype.setCookieLang = function (name) {
+    duoLang.prototype.setCookieLang = function (name) {
         if (name === void 0) { name = this.getLang(); }
         document.cookie = "lang=" + name;
         location.reload();
     };
-    Language.prototype.setLang = function (name) {
+    duoLang.prototype.setLang = function (name) {
         if (name === void 0) { name = this.getLang(); }
         var data_Words = this.Words[name];
         for (var w in data_Words) {
@@ -38,9 +38,9 @@ var Language = /** @class */ (function () {
                 document.querySelector(atr).innerHTML += data_Words[w];
         }
     };
-    Language.prototype.arabicDirection = function (lang) {
+    duoLang.prototype.arabicDirection = function (lang) {
         if (this.getLang() === "ar")
             document.querySelector("body").style.direction = "rtl";
     };
-    return Language;
+    return duoLang;
 }());
